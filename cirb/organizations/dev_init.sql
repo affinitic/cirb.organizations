e55f31353bb2eac8a60d7c27a42e0c1deadb228a
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-02-16 09:00:21 +0100

    I clean unused sql init

--- cirb/organizations/dev_init.sql
+++
@@ -1,8 +0,0 @@
-create database if not exists gscetterbeek;
-use gscetterbeek;
-
-create table if not exists organization (
-    organizationId integer unsigned not null auto_increment primary key,
-    organizationName char(255) not null,
-
-    )engine=sqlite;
