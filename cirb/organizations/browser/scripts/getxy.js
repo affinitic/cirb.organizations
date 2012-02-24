12e66d004104033db47d65fa595bb8b2221d3077
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-02-24 09:31:35 +0100

    search with lang

--- cirb/organizations/browser/scripts/getxy.js
+++ cirb/organizations/browser/scripts/getxy.js
@@ -6,29 +6,26 @@ $(document).ready(function(){
     });
 
     function getaddresses(addr){
-    var language = '';//document.getElementById("l1").value;
-    var address = addr;
-    //var div = document.getElementById("responseGetAddresses");
-    //div.className = "responseWait";
-    //div.innerHTML = "";
-    $(function() {
-        $.ajax({
-            dataType:'jsonp',
-            url: urlService+'Rest/Localize/getaddresses',
-            data: { language:language,address:address },
-            success:function(address_data) {
-                var x =Number(address_data.result[0].point.x);
-                var y =Number(address_data.result[0].point.y);
-                if(!(isNaN(x) || isNaN(y))) {
-                    $('#orga-widgets-x').val(x);
-                    $('#orga-widgets-y').val(y);
-                }
-            },
-            error:function(){
-                $('#orga-widgets-x').val('error');
-            },
+        var language = $('html').attr('lang');
+        var address = addr;
+        $(function() {
+            $.ajax({
+                dataType:'jsonp',
+                url: urlService+'Rest/Localize/getaddresses',
+                data: { language:language,address:address },
+                success:function(address_data) {
+                    var x =Number(address_data.result[0].point.x);
+                    var y =Number(address_data.result[0].point.y);
+                    if(!(isNaN(x) || isNaN(y))) {
+                        $('#orga-widgets-x').val(x);
+                        $('#orga-widgets-y').val(y);
+                    }
+                },
+                error:function(){
+                          $('#orga-widgets-x').val('error');
+                      },
+            });
         });
-    });
 
     };
 });
